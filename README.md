5b11f3dc0ecae4a84f199ea4605fcd71e8b30fd8
Vinay Kumar K V <kvvk.win@gmail.com>
2025-12-29 16:47:14 +0000

    done with this project

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Weather_Forecast_App
-A simple weather forecast application, which can dynamically update the data based on user inputs
+# Weather Forecast App
+A simple web application that retrieves and visualizes weather data for a specified location over a selected number of days.
+
+## Features
+
+- Retrieve forecast data via Open-Meteo API for temperature or relative humidity.
+- Allow user input for location, forecast duration (1-5 days), and data type to display.
+- Display a line chart using Plotly Express to show the time series of weather conditions.
+- Use Streamlit for easy web development with a simple, intuitive interface.
+
+## Setup
+
+1. Install required Python packages:
+   - streamlit
+   - plotly
+   - requests
+   - dotenv (for loading API key from environment variable)
+
+2. Obtain an API key from [geocode.maps.co](https://geocode.maps.co/). Store it as `LOC_API_KEY` in your local `.env` file.
+
+3. Run the app using `streamlit run main.py`
+
+4. Open a web browser and navigate to `http://localhost:8501` to interact with the application.
+
+## Usage
+
+1. Enter a location in the "Place" text field.
+2. Adjust the number of forecast days using the slider.
+3. Select the data type (temperature or relative humidity) from the dropdown menu.
+4. The app will retrieve and display the corresponding weather data for the specified duration, updating the chart accordingly.
+
+*There is scope of adding lot of other features but I have tp proceed and focus on other important projects*
--- main.py
+++ main.py
@@ -1,10 +1,45 @@
 import streamlit as st
+import plotly.express as px
+import os
+import dotenv
+import requests
+import time
 
-st("Weather Forecast for the Next Days")
+
+dotenv.load_dotenv()
+api_key = os.getenv("LOC_API_KEY")
+
+def get_weather_data(p="Bengaluru", d=1 ,op="relative_humidity_2m"):
+    if p == "":
+        p = "Bengaluru"
+    if d == "":
+        d = 1
+    if op == "":
+        op = "relative_humidity_2m"
+    time.sleep(1)
+    url = f"https://geocode.maps.co/search?q={p}&api_key={api_key}" # apikey was gathered from https://geocode.maps.co/
+    location = requests.get(url)
+    longitude = location.json()[0]["lon"]
+    latitude = location.json()[0]["lat"]
+    r = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly={op}&forecast_days={d}")
+    st.success("weather data retrieved", icon="✅")
+    dates = r.json()["hourly"]["time"]
+    t = r.json()["hourly"][op]
+    return dates, t
+
+st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days: ", min_value=1, max_value=5, value=1, help="Select the number of days to forecast")
 option = st.selectbox("Select data to view",
-                      ("Temperature", "Sky"))
+                      ("temperature_2m", "relative_humidity_2m")) #additional attributes can be gathered from https://open-meteo.com/en/docs
 st.subheader(f"{option} for the next {days} days in {place}")
+date, temperatures = get_weather_data(place,days,option)
+figure = px.line(x=date,y=temperatures,labels={'x':'Date','y':f'{option.title()}'})
+st.plotly_chart(figure)
+
+
+
+
+
 
 
